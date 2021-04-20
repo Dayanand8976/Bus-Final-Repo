4260a4980654ab8dc0469ff4f656943d27bd2537
dayanand <dayanandapke@gmail.com>
2021-04-20 15:39:30 +0530

    first commit for sp boot

---
+++ spring-boot/src/main/java/com/lti/controller/HelloRestController.java
@@ -0,0 +1,54 @@
+package com.lti.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+	
+	@GetMapping("/hello")
+	public @ResponseBody String hello() {
+		return "Welcome to Spring Boot ";
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
